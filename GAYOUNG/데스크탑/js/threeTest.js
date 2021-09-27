bb71f91dd9695f0b06506569156e84fd28bed32f
GaYoung0420 <ciks2508@naver.com>
2021-09-27 21:45:13 +0900

    필요없는 코드 삭제

--- GAYOUNG/데스크탑/js/threeTest.js
+++ GAYOUNG/데스크탑/js/threeTest.js
@@ -1,59 +1,60 @@
-import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
 
-import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from "https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js";
 // import Stats from 'three/examples/jsm/libs/stats.module'
 
 function main() {
   var pickedObject;
-  const canvas = document.querySelector('#c');
-  const renderer = new THREE.WebGLRenderer({canvas});
+  const canvas = document.querySelector("#c");
+  const renderer = new THREE.WebGLRenderer({ canvas });
   const scene = new THREE.Scene();
   // 하늘색 배경
   // scene.background = new THREE.Color("#C8EBFA");
 
   const backgroundLoader = new THREE.TextureLoader();
-  const texture = backgroundLoader.load(
-  'model/sunflowers.jpg',
-  () => {
+  const texture = backgroundLoader.load("model/sunflowers.jpg", () => {
     const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
     rt.fromEquirectangularTexture(renderer, texture);
     scene.background = rt.texture;
   });
 
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  );
+  camera.position.set(3, -13, 14);
 
-  const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,1000);
-  camera.position.set(3,-13,14);
-
-
-  const light1 = new THREE.DirectionalLight(0xffffff,5);
+  const light1 = new THREE.DirectionalLight(0xffffff, 5);
   scene.add(light1);
   light1.position.set(0, 0, 500);
 
-  const light2 = new THREE.DirectionalLight(0xffffff,5);
+  const light2 = new THREE.DirectionalLight(0xffffff, 5);
   scene.add(light2);
   light2.position.set(-50, 100, 0);
 
-  const light3 = new THREE.DirectionalLight(0xffffff,5);
+  const light3 = new THREE.DirectionalLight(0xffffff, 5);
   scene.add(light3);
   light3.position.set(50, 100, 0);
 
-  const light4 = new THREE.DirectionalLight(0xffffff,5);
+  const light4 = new THREE.DirectionalLight(0xffffff, 5);
   scene.add(light4);
   light4.position.set(0, 0, -500);
-  
-  
-  const helper1 = new THREE.DirectionalLightHelper(light1);
-  scene.add(helper1);
-  
-  const helper2 = new THREE.DirectionalLightHelper(light2);
-  scene.add(helper2);
 
-  const helper3 = new THREE.DirectionalLightHelper(light3);
-  scene.add(helper3);
+  // DirectionalLightHelper
+  // const helper1 = new THREE.DirectionalLightHelper(light1);
+  // scene.add(helper1);
 
-  const helper4 = new THREE.DirectionalLightHelper(light4);
-  scene.add(helper4);
+  // const helper2 = new THREE.DirectionalLightHelper(light2);
+  // scene.add(helper2);
+
+  // const helper3 = new THREE.DirectionalLightHelper(light3);
+  // scene.add(helper3);
+
+  // const helper4 = new THREE.DirectionalLightHelper(light4);
+  // scene.add(helper4);
 
   // let grid = new THREE.GridHelper(100,20);
   // scene.add(grid);
@@ -61,70 +62,67 @@ function main() {
   var sphere = new Array();
   const loader = new GLTFLoader();
 
-  const controls = new OrbitControls(camera,renderer.domElement);
-  controls.target.set(0,0,0);
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 0, 0);
 
-
-
-
-  loader.load( 'model/blue.glb', function ( gltf ) {
-
-    gltf.scene.position.set(0,0,0);
+  loader.load("model/blue.glb", function (gltf) {
+    gltf.scene.position.set(0, 0, 0);
     let balloon = gltf.scene.clone();
     scene.add(balloon);
     sphere.push(balloon);
-    
   });
 
   var count = 0;
-  let color = ["blue","magenta","yellow","orange","red"];
+  let color = ["blue", "magenta", "yellow", "orange", "red"];
 
   // 물리엔진
-  Ammo().then( start )
-  function start(){
-      //code goes here
+  Ammo().then(start);
+  function start() {
+    //code goes here
   }
 
-  var createSphere = function(pos,color){
+  var createSphere = function (pos, color) {
     // pos -> 벡터값이 매개변수로 들어옴
 
-    loader.load( 'model/'+color+'.glb', function ( gltf ) {
-      gltf.scene.position.set(pos.x,pos.y,pos.z);
-      // gltf.scene.scale.set(70,70,70);
-      // console.log("pos : \n"+pos.x+"\n"+pos.y+"\n"+pos.z)
-      let balloon = gltf.scene.clone();
-      scene.add(balloon);
-      sphere.push(balloon);
-      // console.log(sphere[sphere.length-1].children[0].children)
-  
-    }, undefined, function ( error ) {
-      console.error( error );
-    } );
-    
-  }
+    loader.load(
+      "model/" + color + ".glb",
+      function (gltf) {
+        gltf.scene.position.set(pos.x, pos.y, pos.z);
+        // gltf.scene.scale.set(70,70,70);
+        // console.log("pos : \n"+pos.x+"\n"+pos.y+"\n"+pos.z)
+        let balloon = gltf.scene.clone();
+        scene.add(balloon);
+        sphere.push(balloon);
+        // console.log(sphere[sphere.length-1].children[0].children)
+      },
+      undefined,
+      function (error) {
+        console.error(error);
+      }
+    );
+  };
 
-  var onMouseClick = function(e){
+  var onMouseClick = function (e) {
     console.log(count);
     count++;
-    if(count == 5)
-      count = 0;
+    if (count == 5) count = 0;
     var vec = new THREE.Vector3(); // create once and reuse
     var pos = new THREE.Vector3(); // create once and reuse
 
     vec.set(
-        ( e.clientX / window.innerWidth ) * 2 - 1,
-        - ( e.clientY / window.innerHeight ) * 2 + 1,
-        0.5 );
-    
-    vec.unproject( camera );
-    vec.sub( camera.position ).normalize();
+      (e.clientX / window.innerWidth) * 2 - 1,
+      -(e.clientY / window.innerHeight) * 2 + 1,
+      0.5
+    );
 
-    var distance = - camera.position.z / vec.z;
-    pos.copy( camera.position ).add( vec.multiplyScalar( distance ) );
+    vec.unproject(camera);
+    vec.sub(camera.position).normalize();
 
-    createSphere(pos,color[count]);
+    var distance = -camera.position.z / vec.z;
+    pos.copy(camera.position).add(vec.multiplyScalar(distance));
 
-  }
+    createSphere(pos, color[count]);
+  };
 
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -136,80 +134,15 @@ function main() {
     }
     return needResize;
   }
-/*
-  class PickHelper {
-    constructor() {
-      this.raycaster = new THREE.Raycaster();
-      this.pickedObject = null;
-      this.pickedObjectSavedColor = 0;
-    }
-    pick(normalizedPosition, scene, camera, time) {
-      // restore the color if there is a picked object
-      if (this.pickedObject) {
-        this.pickedObject.material.emissive.setHex(this.pickedObjectSavedColor);
-        this.pickedObject = undefined;
-      }
-     
 
-      // cast a ray through the frustum
-      this.raycaster.setFromCamera(normalizedPosition, camera);
-      // get the list of objects the ray intersected
-      const intersectedObjects = this.raycaster.intersectObjects(scene.children);
-      
-
-      // 물체를 감지한 경우
-      if (intersectedObjects.length) {
-          
-        // pick the first object. It's the closest one
-        this.pickedObject = intersectedObjects[0].object;
-            // 마지막을 제외한 나머지 삭제
-        if(this.pickedObject != sphere[sphere.length - 1])
-          scene.remove(this.pickedObject);
-        // console.log(this.pickedObject.position)
-        // this.pickedObject.position.set(10,10,10);
-      
-      }
-      
-        // this.pickedObject.position.set(rand(-20, 20), rand(-20, 20), rand(-20, 20));
-      
-    }
-  }
-*/
-  const pickPosition = {x: 0, y: 0};
-  // const pickHelper = new PickHelper();
+  const pickPosition = { x: 0, y: 0 };
   clearPickPosition();
-  /*
-  function render(time) {
-    time *= 0.001;  // convert to seconds;
-
-    if (resizeRendererToDisplaySize(renderer)) {
-      const canvas = renderer.domElement;
-      camera.aspect = canvas.clientWidth / canvas.clientHeight;
-      camera.updateProjectionMatrix();
-    }
-
-    // pickHelper.pick(pickPosition, scene, camera, time);
-
-    renderer.render(scene, camera);
-
-    
-    if(sphere != null){
-
-      // 하늘위로 날라가는 애니메이션
-      sphere.forEach(function(value){
-          // value.position.y += 0.1;
-    });
-  }
-    requestAnimationFrame(render);
-  }
-  requestAnimationFrame(render);
-*/
 
   var step = 0;
   //animation
   animation();
-  function animation(time) {     
-    time *= 0.001;  // convert to seconds;
+  function animation(time) {
+    time *= 0.001; // convert to seconds;
 
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
@@ -217,38 +150,31 @@ function main() {
       camera.updateProjectionMatrix();
     }
 
-    if(sphere != null){
-      sphere.forEach(function(value){
-        // step += 0.01;
-        // value.position.x = 2 + ( 10 * (Math.cos(step)));
-        // value.position.y = 2 + ( 10 * Math.abs(Math.sin(step)));
+    if (sphere != null) {
+      sphere.forEach(function (value) {
         value.position.y += 0.1;
         value.rotation.x += 0.02;
-        // value.rotation.y += 0.02;
         value.rotation.z += 0.02;
-        // console.log(value.rotation)
-      }); 
+      });
     }
-      requestAnimationFrame(animation);
-      renderer.render(scene, camera);
+    requestAnimationFrame(animation);
+    renderer.render(scene, camera);
   }
 
-  function animationTest(){
+  function animationTest() {
     pickedObject.rotation.x += 0.02;
     pickedObject.rotation.y += 0.02;
     pickedObject.rotation.z += 0.02;
     pickedObject.position.y += 0.1;
   }
 
-  function animationTest2(){
-    
-    if(sphere != null){
-
+  function animationTest2() {
+    if (sphere != null) {
       // 하늘위로 날라가는 애니메이션
-      sphere.forEach(function(value){
-          // value.position.y += 0.1;
-          value.rotation.x -= 0.02;
-          value.rotation.y += 0.02; 
+      sphere.forEach(function (value) {
+        // value.position.y += 0.1;
+        value.rotation.x -= 0.02;
+        value.rotation.y += 0.02;
       });
     }
   }
@@ -256,42 +182,40 @@ function main() {
   function getCanvasRelativePosition(event) {
     const rect = canvas.getBoundingClientRect();
     return {
-      x: (event.clientX - rect.left) * canvas.width  / rect.width,
-      y: (event.clientY - rect.top ) * canvas.height / rect.height,
+      x: ((event.clientX - rect.left) * canvas.width) / rect.width,
+      y: ((event.clientY - rect.top) * canvas.height) / rect.height,
     };
   }
 
   function setPickPosition(e) {
-      let raycaster = new THREE.Raycaster();
-      const pos = getCanvasRelativePosition(e);
+    let raycaster = new THREE.Raycaster();
+    const pos = getCanvasRelativePosition(e);
 
-      //1. sets the mouse position with a coordinate system where the center
-      //   of the screen is the origin
-      pickPosition.x = (pos.x / canvas.width ) *  2 - 1;
-      pickPosition.y = (pos.y / canvas.height) * -2 + 1;  // note we flip Y
-      
-      //2. set the picking ray from the camera position and mouse coordinates
-      raycaster.setFromCamera( pickPosition, camera );    
+    //1. sets the mouse position with a coordinate system where the center
+    //   of the screen is the origin
+    pickPosition.x = (pos.x / canvas.width) * 2 - 1;
+    pickPosition.y = (pos.y / canvas.height) * -2 + 1; // note we flip Y
 
-      //3. compute intersections (note the 2nd parameter)
-      var intersects = raycaster.intersectObjects( scene.children, true );
-      if(intersects.length > 0){
+    //2. set the picking ray from the camera position and mouse coordinates
+    raycaster.setFromCamera(pickPosition, camera);
 
-        pickedObject = intersects[0].object;
+    //3. compute intersections (note the 2nd parameter)
+    var intersects = raycaster.intersectObjects(scene.children, true);
+    if (intersects.length > 0) {
+      pickedObject = intersects[0].object;
 
-      sphere.forEach(function(value,index){
-      if(pickedObject.uuid == value.children[0].children[0].uuid){
+      sphere.forEach(function (value, index) {
+        if (pickedObject.uuid == value.children[0].children[0].uuid) {
           // value
-      
-          console.log(intersects[0].object.uuid)
+
+          console.log(intersects[0].object.uuid);
           requestAnimationFrame(animationTest);
           // setTimeout(function() { console.log("test"); },1000);
           // requestAnimationFrame(animationTest2);
-      }
-          // scene.remove(value);
-    });
+        }
+        // scene.remove(value);
+      });
     }
-
   }
 
   function clearPickPosition() {
@@ -302,22 +226,27 @@ function main() {
     pickPosition.x = -100000;
     pickPosition.y = -100000;
   }
-  window.addEventListener("click",onMouseClick,false);
-  window.addEventListener('mousemove', setPickPosition);
-  window.addEventListener('mouseout', clearPickPosition);
-  window.addEventListener('mouseleave', clearPickPosition);
 
-  window.addEventListener('touchstart', (event) => {
-    // prevent the window from scrolling
-    event.preventDefault(); 
-    setPickPosition(event.touches[0]);
-  }, {passive: false});
+  window.addEventListener("click", onMouseClick, false);
+  window.addEventListener("mousemove", setPickPosition);
+  window.addEventListener("mouseout", clearPickPosition);
+  window.addEventListener("mouseleave", clearPickPosition);
 
-  window.addEventListener('touchmove', (event) => {
+  window.addEventListener(
+    "touchstart",
+    (event) => {
+      // prevent the window from scrolling
+      event.preventDefault();
+      setPickPosition(event.touches[0]);
+    },
+    { passive: false }
+  );
+
+  window.addEventListener("touchmove", (event) => {
     setPickPosition(event.touches[0]);
   });
 
-  window.addEventListener('touchend', clearPickPosition);
+  window.addEventListener("touchend", clearPickPosition);
 }
 
 main();
