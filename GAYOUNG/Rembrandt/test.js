b1c6ddb0ec561e6a47a478ef166735f8d5f140e0
GaYoung0420 <ciks2508@naver.com>
2021-10-12 17:02:33 +0900

    1012

--- GAYOUNG/Rembrandt/test.js
+++ GAYOUNG/Rembrandt/test.js
@@ -1,23 +1,25 @@
 // particles = new Array();
 particles = [];
 let alpha;
-width = 900;
-height = 200;
+const width = 900;
+const height = 300;
 
 let img;
 
 function setup() {
-  createCanvas(900, 200);
-  image(img, 0, 0, width, height);
+  createCanvas(width, height);
+  // image(img, 0, 0, width, height);
   // size(900, 200);
   background(0);
   noStroke();
   setParticles();
+  console.log(pixels);
 }
 
 function draw() {
   for (particle of particles) {
-    // particle.move();
+    // particle.test();
+    particle.move();
   }
 
   background("black");
@@ -44,52 +46,73 @@ function Particle(xIn, yIn, cIn) {
   this.posX = xIn;
   this.posY = yIn;
   this.color = cIn;
-  this.incr;
-
-  this.test = () => {
-    console.log(this.posX);
-  };
-
-  this.move = function () {
-    this.update();
-    this.wrap();
-    this.display();
-  };
-
-  this.update = function () {
-    this.incr += 0.008;
-    // console.log(this.posX);
-    let theta = noise(this.posX * 0.006, this.posY * 0.004, this.incr) * TWO_PI;
-    // ******
-    // console.log(noise(this.posX * 0.006, this.posY * 0.004, this.incr));
-    this.posX += 2 * cos(theta);
-
-    this.posY += 2 * sin(theta);
-  };
-
-  this.wrap = function () {
-    if (this.posX < 0) this.posX = width;
-    if (this.posX > width) this.posX = 0;
-    if (this.posY < 0) this.posY = height;
-    if (this.posY > height) this.posY = 0;
-  };
-
-  this.display = function () {
-    if (
-      this.posX > 0 &&
-      this.posX < width &&
-      this.posY > 0 &&
-      this.posY < height
-    ) {
-      // console.log("display");
-      pixels[this.posX + this.posY * width] = this.color;
-      // console.log(pixels);
-    }
-  };
+  this.incr = 0;
 }
 
-// Particle.prototype = Object.create(Particle.prototype);
+Particle.prototype = Object.create(Particle.prototype);
 
+Particle.prototype.move = function () {
+  this.update();
+  this.wrap();
+  this.display();
+};
+
+Particle.prototype.update = function () {
+  this.incr += 0.008;
+  // console.log(this.posX);
+  let theta = noise(this.posX * 0.006, this.posY * 0.004, this.incr) * TWO_PI;
+  // ******
+  // console.log(noise(this.posX * 0.006, this.posY * 0.004, this.incr));
+  this.posX += 2 * cos(theta);
+  this.posY += 2 * sin(theta);
+};
+
+Particle.prototype.wrap = function () {
+  if (this.posX < 0) this.posX = width;
+  if (this.posX > width) this.posX = 0;
+  if (this.posY < 0) this.posY = height;
+  if (this.posY > height) this.posY = 0;
+};
+
+Particle.prototype.display = function () {
+  if (
+    this.posX > 0 &&
+    this.posX < width &&
+    this.posY > 0 &&
+    this.posY < height
+  ) {
+    let index = parseInt(this.posX) + parseInt(this.posY) * width;
+    pixels[index] = this.color;
+    // console.log(index);
+    // console.log(pixels[index]);
+    // console.log(pixels[parseInt(this.posX) + parseInt(this.posY)].color);
+    // console.log(pixels[parseInt(this.posX) + parseInt(this.posY) * width]);
+
+    // console.log(pixels);
+  }
+};
+
+function setParticles() {
+  //   particles = new Particle[6000]();
+  for (let i = 0; i < 6000; i++) {
+    let x = random(width);
+    let y = random(height);
+    let adj = map(y, 0, height, 255, 0);
+    // console.log(color(40, adj, 255));
+
+    let c = color(40, adj, 255);
+    // console.log(c);
+    let p = new Particle(x, y, c);
+
+    particles.push(p);
+  }
+}
+
+function mousePressed() {
+  setParticles();
+}
+
+//############################################
 // Particle.prototype.move = function () {
 //   this.update();
 //   this.wrap(this.posX);
@@ -124,22 +147,3 @@ function Particle(xIn, yIn, cIn) {
 //     console.log(pixels);
 //   }
 // };
-
-function setParticles() {
-  //   particles = new Particle[6000]();
-  for (let i = 0; i < 6000; i++) {
-    let x = random(width);
-    let y = random(height);
-    let adj = map(y, 0, height, 255, 0);
-    // console.log(color(40, adj, 255));
-
-    let c = color(40, adj, 255);
-    let p = new Particle(x, y, c);
-
-    particles.push(p);
-  }
-}
-
-function mousePressed() {
-  setParticles();
-}
--- GAYOUNG/chapter1/chapter1.js
+++ GAYOUNG/chapter1/chapter1.js
@@ -1,4 +1,179 @@
-let vertShader = 
+/*
+Magical trail shader
+
+Author: Jason Labbe
+
+Site: jasonlabbe3d.com
+
+Controls:
+	- Move the mouse to create particles.
+	- Hold the middle mouse button to fade away particles.
+	- Press the right mouse button to display the underlying particle system.
+*/
+
+/*
+// If you get an error about max uniforms then you can decrease these 2 values :(
+const MAX_PARTICLE_COUNT = 70;
+const MAX_TRAIL_COUNT = 30;
+
+var colorScheme = ["#E69F66", "#DF843A", "#D8690F", "#B1560D", "#8A430A"];
+var shaded = true;
+var theShader;
+var shaderTexture;
+var trail = [];
+var particles = [];
+
+function preload() {
+  img = loadImage("cow.jpg");
+  theShader = new p5.Shader(this.renderer, vertShader, fragShader);
+}
+
+function setup() {
+  pixelDensity(1);
+
+  let canvas = createCanvas(
+    min(windowWidth, windowHeight),
+    min(windowWidth, windowHeight),
+    WEBGL
+  );
+
+  canvas.canvas.oncontextmenu = () => false; // Removes right-click menu.
+  noCursor();
+
+  shaderTexture = createGraphics(width, height, WEBGL);
+  shaderTexture.noStroke();
+}
+
+function draw() {
+  background(1);
+  noStroke();
+
+  //   snap = createGraphics(100, 100);
+  //   snap.copy(img, 300, 300, 50, 50, 0, 0, 100, 100);
+  //   texture(snap);
+  //   push();
+  //   translate(-100, -100, 0);
+  //   quad(58, 51, 106, 40, 89, 83, 50, 96);
+  //   pop();
+
+  // Trim end of trail.
+  trail.push([mouseX, mouseY]);
+  //   console.log(trail);
+
+  let removeCount = 1;
+  if (mouseIsPressed && mouseButton == CENTER) {
+    removeCount++;
+  }
+
+  for (let i = 0; i < removeCount; i++) {
+    if (trail.length == 0) {
+      break;
+    }
+
+    if (mouseIsPressed || trail.length > MAX_TRAIL_COUNT) {
+      trail.splice(0, 1);
+    }
+  }
+
+  // Spawn particles.
+  if (trail.length > 1 && particles.length < MAX_PARTICLE_COUNT) {
+    let mouse = new p5.Vector(mouseX, mouseY);
+    mouse.sub(pmouseX, pmouseY);
+    if (mouse.mag() > 10) {
+      mouse.normalize();
+      particles.push(new Particle(pmouseX, pmouseY, mouse.x, mouse.y));
+    }
+  }
+
+  translate(-width / 2, -height / 2);
+
+  // Move and kill particles.
+  for (let i = particles.length - 1; i > -1; i--) {
+    particles[i].move();
+    if (particles[i].vel.mag() < 0.1) {
+      particles.splice(i, 1);
+    }
+  }
+
+  if (shaded) {
+    // **** Apply Shader **** //
+    // Display shader.
+    shaderTexture.shader(theShader);
+    let data = serializeSketch();
+    theShader.setUniform("tex", img);
+    theShader.setUniform("resolution", [width, height]);
+    theShader.setUniform("trailCount", trail.length);
+    theShader.setUniform("trail", data.trails);
+    theShader.setUniform("particleCount", particles.length);
+    theShader.setUniform("particles", data.particles);
+    theShader.setUniform("colors", data.colors);
+
+    shaderTexture.rect(0, 0, width, height);
+    texture(shaderTexture);
+    rect(0, 0, width, height);
+  } else {
+    image(img, 0, 0);
+    // Display points.
+    stroke(255, 200, 0);
+    for (let i = 0; i < particles.length; i++) {
+      point(particles[i].pos.x, particles[i].pos.y);
+    }
+
+    // stroke(100, 255, 255);
+    stroke("#FF0000");
+    for (let i = 0; i < trail.length; i++) {
+      point(trail[i][0], trail[i][1]);
+    }
+  }
+}
+
+function mousePressed() {
+  if (mouseButton == RIGHT) {
+    shaded = !shaded;
+  }
+}
+
+function serializeSketch() {
+  data = { trails: [], particles: [], colors: [] };
+
+  for (let i = 0; i < trail.length; i++) {
+    data.trails.push(
+      map(trail[i][0], 0, width, 0.0, 1.0),
+      map(trail[i][1], 0, height, 1.0, 0.0)
+    );
+  }
+
+  for (let i = 0; i < particles.length; i++) {
+    data.particles.push(
+      map(particles[i].pos.x, 0, width, 0.0, 1.0),
+      map(particles[i].pos.y, 0, height, 1.0, 0.0),
+      (particles[i].mass * particles[i].vel.mag()) / 100
+    );
+
+    let itsColor = colorScheme[particles[i].colorIndex];
+    data.colors.push(red(itsColor), green(itsColor), blue(itsColor));
+    // console.log(itsColor);
+  }
+
+  return data;
+}
+
+function Particle(x, y, vx, vy) {
+  this.pos = new p5.Vector(x, y);
+  this.vel = new p5.Vector(vx, vy);
+  this.vel.mult(random(10));
+  this.vel.rotate(radians(random(-25, 25)));
+  this.mass = random(1, 20);
+  this.airDrag = random(0.92, 0.98);
+  this.colorIndex = int(random(colorScheme.length));
+
+  this.move = function () {
+    this.vel.mult(this.airDrag);
+    this.pos.add(this.vel);
+  };
+}
+
+let vertShader = `
 	precision highp float;
 
 	attribute vec3 aPosition;
@@ -8,11 +183,11 @@ let vertShader =
 		positionVec4.xy = positionVec4.xy * 2.0 - 1.0;
 		gl_Position = positionVec4;
 	}
-;
+`;
 
-let fragShader = 
+let fragShader = `
 	precision highp float;
-	
+
 	uniform vec2 resolution;
 	uniform int trailCount;
 	uniform vec2 trail[${MAX_TRAIL_COUNT}];
@@ -30,14 +205,15 @@ let fragShader =
 			for (int i = 0; i < ${MAX_TRAIL_COUNT}; i++) {
 				if (i < trailCount) {
 					vec2 trailPos = trail[i];
-					float value = float(i) / distance(st, trailPos.xy) * 0.00015;  // Multiplier may need to be adjusted if max trail count is tweaked.
-					g += value * 0.5;
-					b += value;
+					float value = float(i) / distance(st, trailPos.xy) * 0.00005;  // Multiplier may need to be adjusted if max trail count is tweaked.
+					r += value;
+					g += value ;
+					b += value * 0.4;
 				}
 			}
 
-			float mult = 0.00005;
-			
+			float mult = 0.00002;
+
 			for (int i = 0; i < ${MAX_PARTICLE_COUNT}; i++) {
 				if (i < particleCount) {
 					vec3 particle = particles[i];
@@ -52,4 +228,38 @@ let fragShader =
 			}
 
 			gl_FragColor = vec4(r, g, b, 1.0);
-};
+	}
+`;
+ */
+
+// 이 변수는 셰이더 객체를 담습니다.
+let theShader;
+// 이 변수는 웹캠 비디오를 담습니다.
+let cam;
+
+function preload() {
+  // 셰이더 불러오기
+  theShader = loadShader("assets/webcam.vert", "assets/webcam.frag");
+}
+
+function setup() {
+  // 셰이더 작동을 위해 WEBGL 모드가 필요합니다.
+  createCanvas(710, 400, WEBGL);
+  noStroke();
+
+  cam = createCapture(VIDEO);
+  cam.size(710, 400);
+
+  cam.hide();
+}
+
+function draw() {
+  // shader()는 활성화 셰이더를 theShader로 설정합니다.
+  shader(theShader);
+
+  // cam을 텍스처로 보내기
+  theShader.setUniform("tex0", cam);
+
+  // rect()함수로 화면에 기하 추가하기
+  rect(0, 0, width, height);
+}
