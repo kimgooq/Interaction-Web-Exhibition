a07e513936d05a79aaca428d0cc0b8f586293682
iyu88 <iyu88887@gmail.com>
2021-11-18 19:50:46 +0900

    Polished popArt

--- HYUNBIN/popArt/index.js
+++ HYUNBIN/popArt/index.js
@@ -1,73 +1,149 @@
-let img = new Image();
-img.src = "../images/img_0.jpeg";
-// img.crossorigin = "Anonymous";
+// const img = new Image();
+// let currentIndex = 0; // 현재 마우스 위치
+// let canvasContexts = []; // 캔버스 이미지 데이터
+// let imageFilters = []; // 미리 필터된 데이터를 저장
+// let container = document.getElementById("container");
+// let cnv_width;
+// let cnv_height;
+// cnv_width;
+// cnv_height;
 
-let currentIndex = 0; // 현재 마우스 위치
-let canvasContexts = []; // 캔버스 이미지 데이터
-let imageFilters = []; // 미리 필터된 데이터를 저장
+const file_icon = document.getElementById("file-icon");
+file_icon.onclick = () => {
+  document.getElementById("img_input").click();
+};
 
-// 빨 0
-// 노 0
-// 초 0
-// 파 0
-// 보 0
+file_icon.onmouseover = () => {
+  icon_status(0);
+};
 
-// 배열로 선언
-// 00 01 02
-// 10 11 12
-// 20 21 22
+file_icon.onmouseout = () => {
+  icon_status(1);
+};
 
-// (A,B) 와 (C,D)
-// 거리 계산 => Math.sqrt( Math.pow( Math.abs(C - A), 2 ) + Math.pow( Math.abs(D - B), 2 ) )
-// 0 1 2
-// 1 2 3
-// 2 3 4
-
-// 매핑하기
-// 0 빨
-// 1 노
-// 2 초
-// 3 파
-// 4 보
-
-document.querySelectorAll("canvas").forEach((el) => {
-  el.addEventListener("mouseover", (e) => {
-    console.log(e.target.classList.value);
-    let here = e.target.classList.value.split(",");
-    // here[1], here[2]
-
-    document.querySelectorAll("canvas").forEach((cnv, i) => {
-      let there = cnv.classList.value.split(",");
-      let d = Math.round(
-        Math.sqrt(
-          Math.pow(Math.abs(there[1] - here[1]), 2) +
-            Math.pow(Math.abs(there[2] - here[2]), 2)
-        )
-      );
-      console.log(d);
-      if (d === 0) {
-        canvasContexts[i].putImageData(imageFilters[0], 0, 0);
-        // canvasContexts[here[0]].putImageData(imageFilters[here[0]], 0, 0);
-      } else if (d === 1) {
-        canvasContexts[i].putImageData(imageFilters[1], 0, 0);
-        // canvasContexts[here[0]].putImageData(imageFilters[here[0]], 0, 0);
-      } else if (d === 2) {
-        canvasContexts[i].putImageData(imageFilters[2], 0, 0);
-        // canvasContexts[here[0]].putImageData(imageFilters[here[0]], 0, 0);
-      } else if (d === 3) {
-        canvasContexts[i].putImageData(imageFilters[3], 0, 0);
-        // canvasContexts[here[0]].putImageData(imageFilters[here[0]], 0, 0);
-      } else if (d === 4) {
-        canvasContexts[i].putImageData(imageFilters[4], 0, 0);
-        // canvasContexts[here[0]].putImageData(imageFilters[here[0]], 0, 0);
-      } else if (d === 5) {
-        canvasContexts[i].putImageData(imageFilters[5], 0, 0);
-        // canvasContexts[here[0]].putImageData(imageFilters[here[0]], 0, 0);
-      }
+document.getElementById("img_input").addEventListener("change", (e) => {
+  img = new Image();
+  currentIndex = 0; // 현재 마우스 위치
+  canvasContexts = []; // 캔버스 이미지 데이터
+  imageFilters = []; // 미리 필터된 데이터를 저장
+  container = document.getElementById("container");
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+  icon_status(1);
+  readImage(e.target);
+  img.addEventListener("load", () => {
+    addCanvases(img.width, img.height);
+    let canvases = document.querySelectorAll("canvas");
+    canvases.forEach((el) => {
+      el.width = cnv_width;
+      el.height = cnv_height;
+      canvasContexts.push(el.getContext("2d"));
     });
+    canvasContexts.forEach((el, i) => {
+      el.drawImage(img, 0, 0, cnv_width, cnv_height);
+    });
+    addFilters();
+    addEventOnCanvas();
   });
 });
 
+const icon_status = (flag) => {
+  if (flag === 0) {
+    file_icon.style.color = "black";
+    file_icon.style.opacity = 1;
+    file_icon.style.transform = "scale(120%)";
+  } else if (flag === 1) {
+    file_icon.style.color = "lightgray";
+    file_icon.style.opacity = 0.5;
+    file_icon.style.transform = "scale(100%)";
+  }
+};
+
+const readImage = (img_input) => {
+  // 인풋 태그에 파일이 있는 경우
+  if (img_input.files && img_input.files[0]) {
+    // FileReader 인스턴스 생성
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      img.src = e.target.result;
+    };
+    reader.readAsDataURL(img_input.files[0]);
+  }
+};
+
+const addCanvases = (width, height) => {
+  cnv_width = Math.floor(window.innerWidth / 9);
+  console.log(cnv_width);
+  cnv_height = Math.floor((height * cnv_width) / width);
+  for (
+    let i = 0;
+    i <= Math.floor(9 * Math.floor(window.innerHeight / cnv_height) + 8);
+    i++
+  ) {
+    let temp_canvas = document.createElement("canvas");
+    temp_canvas.width = cnv_width;
+    temp_canvas.height = cnv_height;
+    temp_canvas.classList.add(`${i},${Math.floor(i / 9)},${i % 9}`);
+    container.appendChild(temp_canvas);
+  }
+};
+
+const addFilters = () => {
+  canvasContexts.forEach((el, i) => {
+    if (i < 10) {
+      let imageData = el.getImageData(0, 0, cnv_width, cnv_height);
+      let editedImageData = imageData;
+      editPixels(editedImageData.data, i);
+      imageFilters.push(editedImageData); // 미리 필터 저장
+    }
+  });
+  console.log(imageFilters);
+};
+
+const addEventOnCanvas = () => {
+  document.querySelectorAll("canvas").forEach((el) => {
+    el.addEventListener("mouseover", (e) => {
+      console.log(e.target.classList.value);
+      let here = e.target.classList.value.split(",");
+      // here[1], here[2]
+
+      document.querySelectorAll("canvas").forEach((cnv, i) => {
+        let there = cnv.classList.value.split(",");
+        let d = Math.round(
+          Math.sqrt(
+            Math.pow(Math.abs(there[1] - here[1]), 2) +
+              Math.pow(Math.abs(there[2] - here[2]), 2)
+          )
+        );
+        console.log(d);
+        if (d === 0) {
+          canvasContexts[i].putImageData(imageFilters[0], 0, 0);
+        } else if (d === 1) {
+          canvasContexts[i].putImageData(imageFilters[1], 0, 0);
+        } else if (d === 2) {
+          canvasContexts[i].putImageData(imageFilters[2], 0, 0);
+        } else if (d === 3) {
+          canvasContexts[i].putImageData(imageFilters[3], 0, 0);
+        } else if (d === 4) {
+          canvasContexts[i].putImageData(imageFilters[4], 0, 0);
+        } else if (d === 5) {
+          canvasContexts[i].putImageData(imageFilters[5], 0, 0);
+        } else if (d === 6) {
+          canvasContexts[i].putImageData(imageFilters[6], 0, 0);
+        } else if (d === 7) {
+          canvasContexts[i].putImageData(imageFilters[7], 0, 0);
+        } else if (d === 8) {
+          canvasContexts[i].putImageData(imageFilters[8], 0, 0);
+        } else if (d === 9) {
+          canvasContexts[i].putImageData(imageFilters[9], 0, 0);
+        }
+      });
+    });
+  });
+};
+
+/*
 function getPixelTotalValue(imgData, i, threshold) {
   let pixel_value =
     0.2126 * imgData[i] + 0.7152 * imgData[i + 1] + 0.0722 * imgData[i + 2];
@@ -82,8 +158,9 @@ function getPixelTotalValue(imgData, i, threshold) {
   }
   // threshold ? 255 : 0;
 }
+*/
 
-// 1. r b g
+/*
 const sP1 = (imgData) => {
   for (let i = 0; i < imgData.length; i += 4) {
     let value = getPixelTotalValue(imgData, i, 100);
@@ -100,253 +177,93 @@ const sP1 = (imgData) => {
     }
   }
 };
+*/
 
-// 2. b r g => 빨강으로
+const sP1 = (imgData) => {
+  for (let i = 0; i < imgData.length; i += 4) {
+    imgData[i] = 195;
+    // imgData[i + 1] = 9;
+    imgData[i + 2] = 31;
+  }
+};
+
 const sP2 = (imgData) => {
   for (let i = 0; i < imgData.length; i += 4) {
-    // let temp1 = imgData[i];
-    // let temp2 = imgData[i + 1];
-    // let temp3 = imgData[i + 2];
-
-    imgData[i] = 255;
-    // imgData[i + 1] = temp1;
-    // imgData[i + 2] = temp2;
+    imgData[i] = 195;
+    // imgData[i + 1] = 9;
+    imgData[i + 2] = 125;
   }
 };
 
-// 3. b g r => 초록으로
 const sP3 = (imgData) => {
   for (let i = 0; i < imgData.length; i += 4) {
-    // let temp1 = imgData[i];
-    // let temp2 = imgData[i + 1];
-    // let temp3 = imgData[i + 2];
-
-    // imgData[i] = temp3;
-    imgData[i + 1] = 255;
-    // imgData[i + 2] = temp1;
+    imgData[i] = 172;
+    // imgData[i + 1] = 9;
+    imgData[i + 2] = 199;
   }
 };
 
-// 4. g r b => 파랑으로
 const sP4 = (imgData) => {
   for (let i = 0; i < imgData.length; i += 4) {
-    // let temp1 = imgData[i];
-    // let temp2 = imgData[i + 1];
-    // let temp3 = imgData[i + 2];
-
-    // imgData[i] = temp2;
-    // imgData[i + 1] = temp1;
-    imgData[i + 2] = 255;
+    imgData[i] = 80;
+    // imgData[i + 1] = 9;
+    imgData[i + 2] = 195;
   }
 };
 
-// 5. g b r => 노랑으로
 const sP5 = (imgData) => {
   for (let i = 0; i < imgData.length; i += 4) {
-    // let temp1 = imgData[i];
-    // let temp2 = imgData[i + 1];
-    // let temp3 = imgData[i + 2];
-
-    imgData[i] = 255;
-    imgData[i + 1] = 255;
-    // imgData[i + 2] = temp1;
+    // imgData[i] = 10;
+    imgData[i + 1] = 31;
+    imgData[i + 2] = 197;
   }
 };
 
-// 6. r g 0 (inv) => 보라
 const sP6 = (imgData) => {
   for (let i = 0; i < imgData.length; i += 4) {
-    // let temp1 = imgData[i];
-    // let temp2 = imgData[i + 1];
-
-    imgData[i] = 128;
-    imgData[i + 2] = 128;
+    // imgData[i] = 10;
+    imgData[i + 1] = 124;
+    imgData[i + 2] = 196;
   }
 };
 
-// 7. 0 r g (inv)
 const sP7 = (imgData) => {
   for (let i = 0; i < imgData.length; i += 4) {
-    let temp1 = imgData[i];
-    let temp2 = imgData[i + 1];
-
-    imgData[i + 1] = temp1 ^ 255;
-    imgData[i + 2] = temp2 ^ 255;
+    // imgData[i] = 8;
+    imgData[i + 1] = 196;
+    imgData[i + 2] = 80;
   }
 };
 
-// 8. b r 0 (inv)
 const sP8 = (imgData) => {
   for (let i = 0; i < imgData.length; i += 4) {
-    let temp1 = imgData[i];
-    let temp3 = imgData[i + 2];
-
-    imgData[i] = temp3 ^ 255;
-    imgData[i + 1] = temp1;
+    imgData[i] = 125;
+    imgData[i + 1] = 196;
+    // imgData[i + 2] = 9;
   }
 };
 
-// 9. 0 b r (inv)
 const sP9 = (imgData) => {
   for (let i = 0; i < imgData.length; i += 4) {
-    let temp1 = imgData[i];
-    let temp3 = imgData[i + 2];
-
-    imgData[i + 1] = temp3 ^ 255;
-    imgData[i + 2] = temp1 ^ 255;
+    imgData[i] = 198;
+    imgData[i + 1] = 174;
+    // imgData[i + 2] = 5;
   }
 };
 
-// 10. b g 0 (inv)
 const sP10 = (imgData) => {
   for (let i = 0; i < imgData.length; i += 4) {
-    let temp2 = imgData[i + 1];
-    let temp3 = imgData[i + 2];
-
-    imgData[i] = temp3 ^ 255;
-    imgData[i + 1] = temp2 ^ 255;
+    imgData[i] = 196;
+    imgData[i + 1] = 81;
+    // imgData[i + 2] = 8;
   }
 };
 
-// 11. 0 b g (inv)
-const sP11 = (imgData) => {
-  for (let i = 0; i < imgData.length; i += 4) {
-    let temp2 = imgData[i + 1];
-    let temp3 = imgData[i + 2];
+let sP_functions = [sP1, sP2, sP3, sP4, sP5, sP6, sP7, sP8, sP9, sP10];
 
-    imgData[i + 1] = temp3 ^ 255;
-    imgData[i + 2] = temp2 ^ 255;
-  }
-};
-
-// 12. r g 0 (inv)
-const sP12 = (imgData) => {
-  for (let i = 0; i < imgData.length; i += 4) {
-    let temp1 = imgData[i];
-    let temp2 = imgData[i + 1];
-
-    imgData[i] = temp1 ^ 255;
-    imgData[i + 1] = temp2 ^ 255;
-  }
-};
-
-// 13. 0 r g (inv)
-const sP13 = (imgData) => {
-  for (let i = 0; i < imgData.length; i += 4) {
-    let temp1 = imgData[i];
-    let temp2 = imgData[i + 1];
-
-    imgData[i + 1] = temp1 ^ 255;
-    imgData[i + 2] = temp2 ^ 255;
-  }
-};
-
-// 14. g r 0 (inv)
-const sP14 = (imgData) => {
-  for (let i = 0; i < imgData.length; i += 4) {
-    let temp1 = imgData[i];
-    let temp2 = imgData[i + 1];
-
-    imgData[i] = temp2 ^ 255;
-    imgData[i + 1] = temp1 ^ 255;
-  }
-};
-
-// 15. 0 g r (inv)
-const sP15 = (imgData) => {
-  for (let i = 0; i < imgData.length; i += 4) {
-    let temp1 = imgData[i];
-    let temp2 = imgData[i + 1];
-
-    imgData[i + 1] = temp2 ^ 255;
-    imgData[i + 2] = temp1 ^ 255;
-  }
-};
-
-// 16. g b 0 (inv)
-const sP16 = (imgData) => {
-  for (let i = 0; i < imgData.length; i += 4) {
-    let temp2 = imgData[i + 1];
-    let temp3 = imgData[i + 2];
-
-    imgData[i] = temp2 ^ 255;
-    imgData[i + 1] = temp3 ^ 255;
-  }
-};
-
-// 17. 0 g b (inv)
-const sP17 = (imgData) => {
-  for (let i = 0; i < imgData.length; i += 4) {
-    let temp2 = imgData[i + 1];
-    let temp3 = imgData[i + 2];
-
-    imgData[i + 1] = temp2 ^ 255;
-    imgData[i + 2] = temp3 ^ 255;
-  }
-};
-
-let sP_functions = [
-  // sP1,
-  sP2, // 빨
-  sP3, // 노
-  sP4, // 초
-  sP5, // 파
-  sP6, // 보
-  // sP7,
-  // sP8,
-  // sP9,
-  // sP10,
-  // sP11,
-  // sP12,
-  // sP13,
-  // sP14,
-  // sP15,
-  // sP16,
-  // sP17,
-];
-
-img.onload = function () {
-  let canvases = document.querySelectorAll("canvas");
-  canvases.forEach((el) => {
-    el.width = img.width;
-    el.height = img.height;
-    canvasContexts.push(el.getContext("2d"));
-  });
-  canvasContexts.forEach((el, i) => {
-    el.drawImage(img, 0, 0);
-    if (i < 6) {
-      let imageData = canvasContexts[i].getImageData(
-        0,
-        0,
-        img.width,
-        img.height
-      );
-      let editedImageData = imageData;
-      editPixels(editedImageData.data, i);
-      imageFilters.push(editedImageData); // 미리 필터 저장
-    }
-  });
-
-  // canvasContexts[4].drawImage(img, 0, 0);
-  // canvasContexts.forEach((el, i) => {
-  //   if (i !== 4) {
-  //     let imageData = canvasContexts[4].getImageData(
-  //       0,
-  //       0,
-  //       img.width,
-  //       img.height
-  //     );
-  //     let editedImageData = imageData;
-  //     editPixels(editedImageData.data);
-  //     el.putImageData(editedImageData, 0, 0);
-  //   }
-  // });
-};
-
-function editPixels(imgData, i) {
+const editPixels = (imgData, i) => {
   const rand_index = Math.floor(Math.random() * sP_functions.length);
-  sP_functions[i % 5](imgData);
-  // sP_functions.splice(rand_index, 1);
+  sP_functions[i % sP_functions.length](imgData);
 
   /*
   let value = getPixelTotalValue(imgData, i, 100);
@@ -354,7 +271,7 @@ function editPixels(imgData, i) {
     imgData[i] = imgData[i + 1] = imgData[i + 2] = value;
   }
   */
-}
+};
 
 /* grayscale 
 function grayscale (pixels, args) {
