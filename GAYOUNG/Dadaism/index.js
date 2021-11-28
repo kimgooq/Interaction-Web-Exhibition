da72881b19b56b13f415697aab030a08d2384509
GaYoung0420 <ciks2508@naver.com>
2021-11-28 11:11:57 +0900

    11-28

--- GAYOUNG/Dadaism/index.js
+++ GAYOUNG/Dadaism/index.js
@@ -4,7 +4,7 @@ const background = new Image();
 background.src = "mona-lisa.jpeg";
 let state = false;
 
-let colorStr = ["red","yellow","black","blue","green"];
+let colorStr = ["red", "yellow", "black", "blue", "green"];
 let activeElement = "";
 let pos = {
   drawable: false,
@@ -14,76 +14,83 @@ let pos = {
 
 // Make sure the image is loaded first otherwise nothing will draw.
 background.onload = function () {
-    canvas.width = background.width;
-    canvas.height = background.height;
+  canvas.width = $("#canvas-background").width();
+  canvas.height = $("#canvas-background").height();
 
-    // ctx.drawImage(background, 0, 0);
+  // ctx.drawImage(background, 0, 0);
+  document.querySelector("#museum_bgm").muted = false;
 
-    //   $("#canvas").addClass("small");
-    $("#background").attr("height", window.innerWidth);
-    $("#background").attr("width", window.innerHeight);
+  $("#canvas").addClass("small");
+  $("#canvas-background").addClass("small");
+  $("#background").attr("height", window.innerWidth);
+  $("#background").attr("width", window.innerHeight);
 
-    canvas.addEventListener("mousedown", listener);
-    canvas.addEventListener("mousemove", listener);
-    canvas.addEventListener("mouseup", listener);
-    canvas.addEventListener("mouseout", listener);
-    canvas.addEventListener("mouseover", listener);
+  $("#capture").attr("height", window.innerWidth);
+  $("#capture").attr("width", window.innerHeight);
 
-    $("#canvas-container").hover(function () {
-        // over
-        moveClose();
-        
-      }, function () {
-        // out
-        moveFar();
-      }
-    );
+  canvas.addEventListener("mousedown", listener);
+  canvas.addEventListener("mousemove", listener);
+  canvas.addEventListener("mouseup", listener);
+  canvas.addEventListener("mouseout", listener);
+  canvas.addEventListener("mouseover", listener);
 
-    $("#eraser").click(()=>{
+  $("#canvas-container").hover(
+    function () {
+      // over
+      moveClose();
+    },
+    function () {
+      // out
+      moveFar();
+    }
+  );
 
-      state = true;
+  $("#eraser").click(() => {
+    state = true;
+  });
 
-    });
+  $("input[name = red]").click(() => {
+    console.log("click");
+  });
+  colorStr.forEach((value) => {
+    colorPick(value);
+  });
 
-    $("input[name = red]").click(()=>{
-        console.log("click")
-    });
-    colorStr.forEach((value)=>{
-      
-      colorPick(value);
-    });
+  $("#camera").click(() => {
+    // 캡쳐 라이브러리를 통해서 canvas 오브젝트를 받고 이미지 파일로 리턴한다.
+    $("#camera").hide();
+    document.querySelector("#camera_effects").muted = false;
+    document.querySelector("#camera_effects").load();
+    document.querySelector("#camera_effects").play();
 
-    $("#camera").click(()=>{
-  // 캡쳐 라이브러리를 통해서 canvas 오브젝트를 받고 이미지 파일로 리턴한다.
-    html2canvas(document.querySelector("#capture")).then(canvas => {
-      saveAs(canvas.toDataURL('image/png'),"capture-test.png");
+    html2canvas(document.querySelector("#capture")).then((canvas) => {
+      saveAs(canvas.toDataURL("image/png"), "capture-test.png");
     });
     function saveAs(uri, filename) {
       // 캡쳐된 파일을 이미지 파일로 내보낸다.
-      var link = document.createElement('a');
-      if (typeof link.download === 'string') {
+
+      var link = document.createElement("a");
+      if (typeof link.download === "string") {
         link.href = uri;
         link.download = filename;
         document.body.appendChild(link);
         link.click();
-      document.body.removeChild(link);
+        document.body.removeChild(link);
+        $("#camera").show();
       } else {
         window.open(uri);
       }
     }
-      
   });
 
-  function colorPick(color){
-    $("#"+color+"Radio").click(()=>{
+  function colorPick(color) {
+    $("#" + color + "Radio").click(() => {
       state = false;
       ctx.strokeStyle = $("." + color).css("background-color");
       console.log(ctx.strokeStyle);
-
+      $("input[type=range]").background;
     });
   }
-
-
 
   function listener(event) {
     switch (event.type) {
@@ -104,13 +111,20 @@ background.onload = function () {
       //   break;
     }
   }
-}
+};
 function moveClose() {
   // console.log("hover");
   $("#canvas").removeClass("small");
   $("#canvas").addClass("large");
+
+  $("#canvas-background").removeClass("small");
+  $("#canvas-background").addClass("large");
+
   $("#background").removeClass("background");
   $("#background").addClass("background-large");
+
+  $("#camera").fadeOut();
+  $("#rangeBar").fadeIn();
   $("#colorpick").fadeIn();
 }
 
@@ -120,8 +134,14 @@ function moveFar() {
   $("#canvas").removeClass("large");
   $("#canvas").addClass("small");
 
+  $("#canvas-background").removeClass("large");
+  $("#canvas-background").addClass("small");
+
   $("#background").removeClass("background-large");
   $("#background").addClass("background");
+
+  $("#camera").fadeIn();
+  $("#rangeBar").fadeOut();
   $("#colorpick").fadeOut();
 }
 
@@ -147,11 +167,11 @@ function initDraw(event) {
 
 function draw(event) {
   let coors = getMousePos(event);
-  console.log(state)
-  if(state == true){
-    ctx.globalCompositeOperation="destination-out";
-  }else{
-    ctx.globalCompositeOperation="source-over";
+  console.log(state);
+  if (state == true) {
+    ctx.globalCompositeOperation = "destination-out";
+  } else {
+    ctx.globalCompositeOperation = "source-over";
   }
 
   ctx.lineTo(coors.x, coors.y);
@@ -173,3 +193,56 @@ function getPosition(event) {
 }
 
 //***** *****/
+$("input[type=range]").wrap("<div class='range'></div>");
+var i = 1;
+
+$(".range").each(function () {
+  var n = this.getElementsByTagName("input")[0].value;
+  var x =
+    (n / 50) * (this.getElementsByTagName("input")[0].offsetWidth - 8) - 12;
+  this.id = "range" + i;
+  console.log(this.id);
+  this.className = "range";
+
+  this.innerHTML +=
+    "<style>#" +
+    this.id +
+    ":hover input[type=range]:before{content:'" +
+    n +
+    "'!important;left: " +
+    x +
+    "px;} #" +
+    this.id +
+    ":hover input[type=range]:after{left: " +
+    x +
+    "px}</style>";
+  i++;
+});
+
+$("input[type=range]").on("input", function () {
+  var a = this.value;
+  var p = (a / 50) * (this.offsetWidth - 8) - 12;
+  console.log(a);
+  ctx.lineWidth = a;
+
+  this.parentNode.className = "range";
+
+  this.parentNode.getElementsByTagName("style")[0].innerHTML +=
+    "#" +
+    this.parentNode.id +
+    " input[type=range]::-webkit-slider-runnable-track {background:linear-gradient(to right, #803fb5 0%, #803fb5 " +
+    a * 2 +
+    "%, #ffffff " +
+    a +
+    "%)} #" +
+    this.parentNode.id +
+    ":hover input[type=range]:before{content:'" +
+    a +
+    "'!important;left: " +
+    p +
+    "px;} #" +
+    this.parentNode.id +
+    ":hover input[type=range]:after{left: " +
+    p +
+    "px}";
+});
